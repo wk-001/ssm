39f9c7e47a5a9f71a35c0246b0498d35fa371cd3
wk-001 <2326699650@qq.com>
2019-08-20 09:51:38 +0800

    添加用户校验

---
+++ src/main/java/com/wk/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wk.service.impl;
+
+import com.wk.dao.DepartmentMapper;
+import com.wk.pojo.Department;
+import com.wk.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+	@Autowired
+	private DepartmentMapper departmentMapper;
+
+	@Override
+	public List<Department> getDepts() {
+		return departmentMapper.selectByExample(null);
+	}
+}
